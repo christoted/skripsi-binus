de66b7a0b095473f82b5f39676bb4bcb9e1eff2c
Luis Anthonie Alkins <46640013+luisanthoniealkins@users.noreply.github.com>
2022-03-15 12:44:00 +0700

    git add navigation to subject detail

--- app/src/main/java/com/example/project_skripsi/module/student/main/stclass/SubjectAdapter.kt
+++ app/src/main/java/com/example/project_skripsi/module/student/main/stclass/SubjectAdapter.kt
@@ -1,9 +1,15 @@
 package com.example.project_skripsi.module.student.main.stclass
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView.*
+import com.example.project_skripsi.R
 import com.example.project_skripsi.databinding.ItemStClassSubjectBinding
+import com.example.project_skripsi.module.student.subject_detail.StSubjectActivity.Companion.EXTRA_SUBJECT_NAME
 
 
 class SubjectAdapter(private val subjectList: List<String>) :
@@ -20,11 +26,17 @@ class SubjectAdapter(private val subjectList: List<String>) :
     override fun getItemCount() = subjectList.size
 
     class SubjectViewHolder ( private val binding : ItemStClassSubjectBinding) : ViewHolder(binding.root) {
-
         fun bind(subjectName: String) {
             with(binding) {
                 this.textviewSubjectName.text = subjectName
             }
+
+            binding.root.setOnClickListener { view ->
+                val toStSubjectActivity = StClassFragmentDirections.actionNavigationClassToStSubjectActivity()
+                toStSubjectActivity.subjectName = subjectName
+                view.findNavController().navigate(toStSubjectActivity)
+            }
         }
     }
+
 }
