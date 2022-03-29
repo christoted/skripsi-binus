8ae2e9859f9005cce99ee9c8d49891bf3b75586c
Luis Anthonie Alkins <46640013+luisanthoniealkins@users.noreply.github.com>
2022-03-29 23:09:48 +0700

    cleaning

--- app/src/main/java/com/example/project_skripsi/core/model/firestore/HomeSectionData.kt
+++ app/src/main/java/com/example/project_skripsi/core/model/firestore/HomeSectionData.kt
@@ -1,3 +1,5 @@
 package com.example.project_skripsi.core.model.firestore
 
 open class HomeSectionData { }
+
+data class HomeMainSection(val sectionName: String, val sectionItem: List<HomeSectionData>)
