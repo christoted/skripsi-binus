056265160f6afeb94c597386802b3167bb54a880
Luis Anthonie Alkins <46640013+luisanthoniealkins@users.noreply.github.com>
2022-04-18 21:26:59 +0700

    Update FireRepository.kt (#52)

--- app/src/main/java/com/example/project_skripsi/core/repository/FireRepository.kt
+++ app/src/main/java/com/example/project_skripsi/core/repository/FireRepository.kt
@@ -26,6 +26,18 @@ class FireRepository : OnSuccessListener<Void>, OnFailureListener {
         const val COLLECTION_RESOURCE = "resources"
         const val COLLECTION_ANNOUNCEMENT = "announcements"
         const val COLLECTION_SCHOOL = "schools"
+
+        val mapCollection = mapOf(
+            Student::class to COLLECTION_STUDENT,
+            Teacher::class to COLLECTION_TEACHER,
+            Parent::class to COLLECTION_PARENT,
+            Administrator::class to COLLECTION_ADMINISTRATOR,
+            StudyClass::class to COLLECTION_STUDY_CLASS,
+            TaskForm::class to COLLECTION_TASK_FORM,
+            Resource::class to COLLECTION_RESOURCE,
+            Announcement::class to COLLECTION_ANNOUNCEMENT,
+            School::class to COLLECTION_SCHOOL,
+        )
     }
 
     @Deprecated("Replaced by getItem")
@@ -186,18 +198,7 @@ class FireRepository : OnSuccessListener<Void>, OnFailureListener {
         val exception = MutableLiveData<Exception>()
         val db = FirebaseFirestore.getInstance()
 
-        when(T::class) {
-            Student::class -> COLLECTION_STUDENT
-            Teacher::class -> COLLECTION_TEACHER
-            Parent::class -> COLLECTION_PARENT
-            Administrator::class -> COLLECTION_ADMINISTRATOR
-            StudyClass::class -> COLLECTION_STUDY_CLASS
-            TaskForm::class -> COLLECTION_TASK_FORM
-            Resource::class -> COLLECTION_RESOURCE
-            Announcement::class -> COLLECTION_ANNOUNCEMENT
-            School::class -> COLLECTION_SCHOOL
-            else -> null
-        }?.let { collection ->
+        mapCollection[T::class]?.let { collection ->
             db.collection(collection)
                 .document(uid)
                 .get()
@@ -228,6 +229,24 @@ class FireRepository : OnSuccessListener<Void>, OnFailureListener {
         return Pair(data, exception)
     }
 
+    inline fun <reified T> getAllItems() : Pair<LiveData<List<T>>, LiveData<Exception>> {
+        val list = mutableListOf<T>()
+        val data = MutableLiveData<List<T>>()
+        val exception = MutableLiveData<Exception>()
+
+        val db = FirebaseFirestore.getInstance()
+        mapCollection[T::class]?.let { collection ->
+            db.collection(collection)
+                .get()
+                .addOnSuccessListener { result ->
+                    result.map { document -> list.add(document.toObject(T::class.java)) }
+                    data.postValue(list)
+                }
+                .addOnFailureListener { ex -> exception.postValue(ex)}
+        }
+        return Pair(data, exception)
+    }
+
 
     fun alterItems(items : List<Any>) : Pair<LiveData<Boolean>, LiveData<Exception>> {
         val isSuccess = MutableLiveData<Boolean>()
@@ -235,38 +254,28 @@ class FireRepository : OnSuccessListener<Void>, OnFailureListener {
 
         var successCounter = 0
         items.map {
-            db.collection(
-                when (it) {
-                    is Student -> COLLECTION_STUDENT
-                    is Teacher -> COLLECTION_TEACHER
-                    is Parent -> COLLECTION_PARENT
-                    is Administrator -> COLLECTION_ADMINISTRATOR
-                    is StudyClass -> COLLECTION_STUDY_CLASS
-                    is TaskForm -> COLLECTION_TASK_FORM
-                    is Resource -> COLLECTION_RESOURCE
-                    is Announcement -> COLLECTION_ANNOUNCEMENT
-                    else -> COLLECTION_SCHOOL
-                }
-            ).document(
-                when (it) {
-                    is Student -> it.id!!
-                    is Teacher -> it.id!!
-                    is Parent -> it.id!!
-                    is Administrator -> it.id!!
-                    is StudyClass -> it.id!!
-                    is TaskForm -> it.id!!
-                    is Resource -> it.id!!
-                    is Announcement -> it.id!!
-                    is School -> it.id!!
-                    else -> "null"
-                }
-            ).set(
-                it
-            ).addOnSuccessListener {
-                successCounter++
-                if (successCounter == items.size) isSuccess.postValue(true)
-            }.addOnFailureListener{ _exception ->
-                exception.postValue(_exception)
+            mapCollection[it::class]?.let { collection ->
+                db.collection(collection)
+                    .document(
+                        when (it) {
+                            is Student -> it.id!!
+                            is Teacher -> it.id!!
+                            is Parent -> it.id!!
+                            is Administrator -> it.id!!
+                            is StudyClass -> it.id!!
+                            is TaskForm -> it.id!!
+                            is Resource -> it.id!!
+                            is Announcement -> it.id!!
+                            is School -> it.id!!
+                            else -> "null"
+                        }
+                    )
+                    .set(it)
+                    .addOnSuccessListener {
+                        successCounter++
+                        if (successCounter == items.size) isSuccess.postValue(true)
+                    }
+                    .addOnFailureListener { _exception -> exception.postValue(_exception) }
             }
         }
         return Pair(isSuccess, exception)
