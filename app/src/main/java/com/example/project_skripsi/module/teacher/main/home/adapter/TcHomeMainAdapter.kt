abbed6521743697c9277df7d955c55effce3541d
Christopher Teddy Mienarto <46671469+christoted@users.noreply.github.com>
2022-04-20 23:25:11 +0700

    Tc/revision home (#55)
    * Squashed commit of the following:
    
    commit 7337bfcacb55dfc606e52c1e1b95bd098e1f8d70
    Author: Luis Anthonie Alkins <46640013+luisanthoniealkins@users.noreply.github.com>
    Date:   Wed Apr 20 08:15:02 2022 +0700
    
        add parent base (#54)
    
    * Update lil changes
    
    * refactor-adapter
    
    * feat/home
    
    * Add Date Helper function

--- app/src/main/java/com/example/project_skripsi/module/teacher/main/home/adapter/TcHomeMainAdapter.kt
+++ app/src/main/java/com/example/project_skripsi/module/teacher/main/home/adapter/TcHomeMainAdapter.kt
@@ -1,4 +1,47 @@
 package com.example.project_skripsi.module.teacher.main.home.adapter
 
-class TcHomeMainAdapter {
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.project_skripsi.core.model.local.HomeMainSection
+import com.example.project_skripsi.databinding.ItemTcHomeMainSectionBinding
+import com.example.project_skripsi.module.teacher._sharing.agenda.TcAgendaItemListener
+
+class TcHomeMainAdapter(private val listHomeSectionData: List<HomeMainSection>, val listener: TcAgendaItemListener):
+    RecyclerView.Adapter<TcHomeMainAdapter.TcHomeMainSectionViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TcHomeMainSectionViewHolder {
+        return  TcHomeMainSectionViewHolder(
+            ItemTcHomeMainSectionBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: TcHomeMainSectionViewHolder, position: Int) {
+        listHomeSectionData.let {
+            val singleItemMainSection = it[position]
+            // Declare the child adapter
+            val childAdapter = TcHomeChildAdapter(singleItemMainSection, listener)
+            holder.bind(singleItemMainSection, childAdapter)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listHomeSectionData.size
+    }
+
+    inner class TcHomeMainSectionViewHolder(private val binding: ItemTcHomeMainSectionBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(singleHomeMainSectionItem: HomeMainSection, adapter: TcHomeChildAdapter) {
+            with(binding) {
+                sectionTitle.text = singleHomeMainSectionItem.sectionName
+                sectionTitleCount.text = singleHomeMainSectionItem.sectionItem.count().toString()
+                Log.d("000", "bind: ${singleHomeMainSectionItem.sectionItem.count()}")
+                with(binding.sectionItemsRecyclerView) {
+                    sectionItemsRecyclerView.layoutManager = LinearLayoutManager(context)
+                    sectionItemsRecyclerView.adapter = adapter
+                }
+            }
+        }
+    }
 }
