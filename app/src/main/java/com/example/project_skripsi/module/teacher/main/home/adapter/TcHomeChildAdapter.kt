eaf5063a7c8479e81c32598ce078b000587cf054
ted <vitedocorp@yahoo.com>
2022-04-20 12:28:01 +0700

    refactor-adapter

--- app/src/main/java/com/example/project_skripsi/module/teacher/main/home/adapter/TcHomeChildAdapter.kt
+++ app/src/main/java/com/example/project_skripsi/module/teacher/main/home/adapter/TcHomeChildAdapter.kt
@@ -1,4 +1,54 @@
 package com.example.project_skripsi.module.teacher.main.home.adapter
 
-class TcHomeChildAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.project_skripsi.core.model.local.HomeMainSection
+import com.example.project_skripsi.databinding.ItemStHomeSectionAnnouncementBinding
+import com.example.project_skripsi.databinding.ItemStHomeSectionPaymentBinding
+import com.example.project_skripsi.databinding.ItemTcAgendaGeneralBinding
+import com.example.project_skripsi.module.student.main._sharing.StHomeAnnouncementViewHolder
+import com.example.project_skripsi.module.student.main._sharing.StHomePaymentViewHolder
+import com.example.project_skripsi.module.student.main.calendar.StCalendarViewModel
+import com.example.project_skripsi.module.student.main.home.view.adapter.ItemListener
+import com.example.project_skripsi.module.teacher._sharing.agenda.TcAgendaAssignmentViewHolder
+import com.example.project_skripsi.module.teacher._sharing.agenda.TcAgendaExamViewHolder
+import com.example.project_skripsi.module.teacher._sharing.agenda.TcAgendaItemListener
+import com.example.project_skripsi.module.teacher._sharing.agenda.TcAgendaMeetingViewHolder
+import com.example.project_skripsi.utils.Constant
+
+class TcHomeChildAdapter(val item: HomeMainSection, private val listener: TcAgendaItemListener) :
+RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        when (item.sectionName) {
+            Constant.SECTION_MEETING -> TcAgendaMeetingViewHolder(
+                ItemTcAgendaGeneralBinding.inflate(
+                    LayoutInflater.from(viewGroup.context), viewGroup, false), listener)
+            Constant.SECTION_EXAM -> TcAgendaExamViewHolder(
+                ItemTcAgendaGeneralBinding.inflate(
+                    LayoutInflater.from(viewGroup.context), viewGroup, false),listener)
+            Constant.SECTION_ASSIGNMENT -> TcAgendaAssignmentViewHolder(
+                ItemTcAgendaGeneralBinding.inflate(
+                    LayoutInflater.from(viewGroup.context), viewGroup, false),listener)
+            Constant.SECTION_PAYMENT -> StHomePaymentViewHolder(
+                ItemStHomeSectionPaymentBinding.inflate(
+                    LayoutInflater.from(viewGroup.context), viewGroup, false))
+            else -> StHomeAnnouncementViewHolder(
+                ItemStHomeSectionAnnouncementBinding.inflate(
+                    LayoutInflater.from(viewGroup.context), viewGroup, false))
+        }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val singleData = item.sectionItem[position]
+        when (item.sectionName) {
+            Constant.SECTION_MEETING -> (holder as TcAgendaMeetingViewHolder).bind(singleData)
+            Constant.SECTION_EXAM  -> (holder as TcAgendaExamViewHolder).bind(singleData)
+            Constant.SECTION_ASSIGNMENT -> (holder as TcAgendaAssignmentViewHolder).bind(singleData)
+            Constant.SECTION_PAYMENT -> (holder as StHomePaymentViewHolder).bind(singleData)
+            else -> (holder as StHomeAnnouncementViewHolder).bind(singleData)
+        }
+    }
+
+    override fun getItemCount(): Int = item.sectionItem.size
+
 }
--- app/src/main/java/com/example/project_skripsi/module/teacher/main/home/adapter/TcHomeMainAdapter.kt
+++ app/src/main/java/com/example/project_skripsi/module/teacher/main/home/adapter/TcHomeMainAdapter.kt
@@ -1,4 +1,51 @@
 package com.example.project_skripsi.module.teacher.main.home.adapter
 
-class TcHomeMainAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.project_skripsi.core.model.local.HomeMainSection
+import com.example.project_skripsi.databinding.ItemStHomeMainSectionBinding
+import com.example.project_skripsi.databinding.ItemTcHomeMainSectionBinding
+import com.example.project_skripsi.module.student.main.home.view.adapter.ItemListener
+import com.example.project_skripsi.module.student.main.home.view.adapter.StHomeRecyclerViewChildAdapter
+import com.example.project_skripsi.module.student.main.home.view.adapter.StHomeRecyclerViewMainAdapter
+import com.example.project_skripsi.module.teacher._sharing.agenda.TcAgendaItemListener
+
+class TcHomeMainAdapter(val listHomeSectionData: List<HomeMainSection>, val listener: TcAgendaItemListener):
+    RecyclerView.Adapter<TcHomeMainAdapter.TcHomeMainSectionViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TcHomeMainSectionViewHolder {
+        return  TcHomeMainSectionViewHolder(
+            ItemTcHomeMainSectionBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: TcHomeMainSectionViewHolder, position: Int) {
+        listHomeSectionData.let {
+            val singleItemMainSection = it[position]
+            // Declare the child adapter
+            val childAdapter = TcHomeChildAdapter(singleItemMainSection, listener)
+            holder.bind(singleItemMainSection, childAdapter)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listHomeSectionData.size ?: 0
+    }
+
+
+    inner class TcHomeMainSectionViewHolder(private val binding: ItemTcHomeMainSectionBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(singleHomeMainSectionItem: HomeMainSection, adapter: TcHomeChildAdapter) {
+            with(binding) {
+                sectionTitle.text = singleHomeMainSectionItem.sectionName
+                sectionTitleCount.text = singleHomeMainSectionItem.sectionItem.count().toString()
+                with(binding.sectionItemsRecyclerView) {
+                    sectionItemsRecyclerView.layoutManager = LinearLayoutManager(context)
+                    sectionItemsRecyclerView.adapter = adapter
+                }
+            }
+        }
+    }
 }
