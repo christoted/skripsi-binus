74a1c8cdbb24c8329e210b3e64c175736121f531
Luis Anthonie Alkins <46640013+luisanthoniealkins@users.noreply.github.com>
2022-06-19 17:08:52 +0700

    Parent/progress (#69)
    * add student progress
    
    * finalize home
    
    * finishing design parent
    
    * finalize student
    
    * update ui
    
    * update
    
    * update
    
    * migrate to one activity architecture
    
    * update phase 4
    
    * commit
    
    * additional update
    
    * finishing task
    
    * push update
    
    * push temp
    
    * add full feature
    
    * push update
    
    * fix bug
    
    * add preq
    
    * push before zoom

--- app/src/main/java/com/example/project_skripsi/module/student/task/form/StTaskFormViewModel.kt
+++ app/src/main/java/com/example/project_skripsi/module/student/task/form/StTaskFormViewModel.kt
@@ -5,10 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.project_skripsi.core.model.firestore.AssignedTaskForm
-import com.example.project_skripsi.core.model.firestore.Student
-import com.example.project_skripsi.core.model.firestore.StudyClass
-import com.example.project_skripsi.core.model.firestore.TaskForm
+import com.example.project_skripsi.core.model.firestore.*
 import com.example.project_skripsi.core.model.local.AssignedQuestion
 import com.example.project_skripsi.core.model.local.TaskFormStatus
 import com.example.project_skripsi.core.model.local.TaskFormTimer
@@ -17,7 +14,9 @@ import com.example.project_skripsi.core.repository.FireRepository
 import com.example.project_skripsi.utils.Constant.Companion.TASK_FORM_ESSAY
 import com.example.project_skripsi.utils.Constant.Companion.TASK_FORM_MC
 import com.example.project_skripsi.utils.Constant.Companion.isExam
+import com.example.project_skripsi.utils.generic.GenericLinkHandler
 import com.example.project_skripsi.utils.generic.GenericObserver.Companion.observeOnce
+import com.example.project_skripsi.utils.generic.HandledEvent
 import com.example.project_skripsi.utils.helper.DateHelper
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -48,6 +47,12 @@ class StTaskFormViewModel : ViewModel() {
 
     private val _isSubmitted = MutableLiveData<Boolean>()
     val isSubmitted: LiveData<Boolean> = _isSubmitted
+
+    private val _incompleteResource = MutableLiveData<HandledEvent<Resource>>()
+    val incompleteResource : LiveData<HandledEvent<Resource>> = _incompleteResource
+
+    private val _incompleteTask = MutableLiveData<HandledEvent<AssignedTaskForm>>()
+    val incompleteTask : LiveData<HandledEvent<AssignedTaskForm>> = _incompleteTask
 
     var isViewOnly = false
     var taskFormId = ""
@@ -85,31 +90,50 @@ class StTaskFormViewModel : ViewModel() {
         FireRepository.inst.getItem<Student>(uid).first.observeOnce { student ->
             curStudent = student
 
-            var assignedTaskForm: AssignedTaskForm? = null
+
+            var incompleteId: String? = null
+            var incompleteTask: AssignedTaskForm? = null
             taskForm.value?.let { task ->
                 task.prerequisiteResources?.map {
-
+                    curStudent.completedResources?.let { list ->
+                        if (!list.contains(it)) {
+                            incompleteId = it
+                            return@map
+                        }
+                    }
                 }
 
+                incompleteId?.let { id ->
+                    FireRepository.inst.getItem<Resource>(id).first.observeOnce {
+                        _incompleteResource.postValue(HandledEvent(it))
+                    }
+                }
 
-                when {
-                    isExam(task.type) -> student.assignedExams
-                    else -> student.assignedAssignments
-                }?.firstOrNull { it.id == taskFormId }?.let { assignedTaskForm = it }
+                task.prerequisiteTaskForms?.map { id ->
+                    curStudent.assignedAssignments?.firstOrNull { it.id == id }?.let {
+                        if (it.isSubmitted == false) {
+                            incompleteTask = it
+                            _incompleteTask.postValue(HandledEvent(it))
+                            return@map
+                        }
+                    }
+                }
             }
+            if (incompleteId != null || incompleteTask != null) return@observeOnce
 
             student.studyClass?.let { loadStudyClass(it) }
 
+            val assignedTaskForm = taskForm.value?.let { task ->
+                when {
+                    isExam(task.type) -> student.assignedExams
+                    else -> student.assignedAssignments
+                }?.firstOrNull { it.id == taskFormId }
+            }
+
             _formStatus.postValue(
                 Pair(
-                    TaskFormStatus.getStatus(
-                        taskForm.value!!,
-                        assignedTaskForm!!
-                    ),
-                    TaskFormStatus.getStatusColor(
-                        taskForm.value!!,
-                        assignedTaskForm!!
-                    )
+                    TaskFormStatus.getStatus(taskForm.value!!, assignedTaskForm!!),
+                    TaskFormStatus.getStatusColor(taskForm.value!!, assignedTaskForm)
                 )
             )
 
@@ -118,7 +142,7 @@ class StTaskFormViewModel : ViewModel() {
                 questionList.add(
                     AssignedQuestion(
                         question,
-                        assignedTaskForm?.answers?.getOrNull(index)
+                        assignedTaskForm.answers?.getOrNull(index)
                     )
                 )
             }
@@ -133,8 +157,8 @@ class StTaskFormViewModel : ViewModel() {
     fun submitAnswer(newAnswer: List<Pair<String, List<String>>>) {
         taskForm.value?.let { task ->
             when {
-                isExam(task.type) -> curStudent.assignedAssignments
-                else -> curStudent.assignedExams
+                isExam(task.type) -> curStudent.assignedExams
+                else -> curStudent.assignedAssignments
             }?.firstOrNull { it.id == taskFormId }?.let {
                 it.isSubmitted = true
                 it.answers?.mapIndexed { index, answer ->
